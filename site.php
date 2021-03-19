b7de93d56fcd989349eb9c370db451c04823b1b0
K-Zimny <kenneth.zimny@gmail.com>
2021-03-19 14:00:49 -0500

    Associate Arrays

---
+++ site.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="site.php" method="post">
+        <input type="text" name="student">
+        <input type="submit">
+    </form>
+
+    <?php
+        $grades=[
+            "Jim" => "A+",
+            "Pam" => "B-",
+            "Oscar" => "C+" 
+        ];
+        echo $grades[$_POST["student"]];
+        // echo $grades["Jim"];
+        
+    ?>
+
+</body>
+
+</html>
